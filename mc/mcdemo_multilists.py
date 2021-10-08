ef44ae5779f5c1f11ee44f954b86e2f61ea073a8
Danie24463git config --global user.name Danie24463 <you@example.com>
2021-10-08 15:36:44 +0100

    added multilist

---
+++ mc/mcdemo_multilists.py
@@ -0,0 +1,30 @@
+#Daniel Opoku
+#Mc demo lists
+
+from mcpi.minecraft import Minecraft
+mc = Minecraft.create()
+
+woolLine= [13,12,8,7,1]
+woolGrid= [[0,15,15,15,15,15,15,15,],
+           [15,5,15,15,15,15,5,15],
+           [15,5,15,15,15,15,5,15],
+           [15,5,5,5,5,5,5,15],
+           [15,5,5,5,5,5,5,15],
+           [15,5,5,5,5,5,5,15],
+           [15,5,5,5,5,5,5,15],
+           [15,5,0,0,0,0,5,15],
+           [15,5,0,0,0,0,5,15],
+           [15,5,5,5,5,5,5,15],
+           [15,15,15,15,15,15,15,15,]]
+pos =mc.player.getTilePos()
+
+for i, wool in enumerate (woolLine):
+    print (str(i) + "is the color" + str (wool))
+    mc.setBlock (pos.x+i, pos.y, pos.z, 35, wool)
+   
+    
+for i, row in enumerate (woolGrid):
+    print(row)
+    for j, col in enumerate(row):
+        print(col)
+        mc.setBlock (pos.x+j, pos.y+i, pos.z, 35, col)
